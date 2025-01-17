47be6007563847040f864bf873b99bf529fa48be
root <cooperg122@gmail.com>
2025-01-17 14:00:42 -0700

    finished composition poly

--- src/prover/composition_poly.rs
+++ src/prover/composition_poly.rs
@@ -2,11 +2,31 @@ use crate::finite_field::field_params::Fp;
 use crate::ff::{PrimeField, Field};
 use crate::polynomials::poly::Polynomial;
 
-pub fn calculate_composition_poly(c_1: &Polynomial<Fp>, c_2: &Polynomial<Fp>, c_3: &Polynomial<Fp>, g: &Fp) {
-    calculate_basic_composition_poly(c_1, &Fp::ONE, 1); // divisor is x - g^0
-    calculate_basic_composition_poly(c_2, &Fp::ONE, 128); // divisor is x^128 - 1
+use rand::rngs::OsRng; // remove this once root hash is used for randomness
+
+
+pub fn calculate_composition_poly(c_1: &Polynomial<Fp>, c_2: &Polynomial<Fp>, c_3: &Polynomial<Fp>, g: &Fp) -> Polynomial<Fp> {
+    let p_1 = calculate_basic_composition_poly(c_1, &Fp::ONE, 1); // divisor is x - g^0
+    let p_2 = calculate_basic_composition_poly(c_2, &Fp::ONE, 128); // divisor is x^128 - 1
     let last_subgroup_element = g.pow(&[127 as u64]);
-    calculate_basic_composition_poly(c_3, &last_subgroup_element, 1); // divisor is x - g^127
+    let p_3 = calculate_basic_composition_poly(c_3, &last_subgroup_element, 1); // divisor is x - g^127
+
+    // Use the root hash of the commitment to f to get three random nums.
+    let a_as_vec = vec![Fp::random(OsRng)];
+    let b_as_vec = vec![Fp::random(OsRng)];
+    let c_as_vec = vec![Fp::random(OsRng)];
+
+    let a_poly = Polynomial::new(&a_as_vec);
+    let b_poly = Polynomial::new(&b_as_vec);
+    let c_poly = Polynomial::new(&c_as_vec);
+
+    let scaled_p_1 = p_1.mul(&a_poly);
+    let scaled_p_2 = p_2.mul(&b_poly);
+    let scaled_p_3 = p_3.mul(&c_poly);
+
+    let composition_poly = scaled_p_1.add(&scaled_p_2).add(&scaled_p_3);
+
+    composition_poly
 }
 
 // Creates a basic quotient polynomial by dividing a given polynomial by one root (x^x_degree - constant) for a given constant.
